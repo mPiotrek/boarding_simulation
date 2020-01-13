3e2b1ea1420daec97e601b9e5c5d28197ead42d8
mPiotrek <59826203+mPiotrek@users.noreply.github.com>
2020-01-13 20:20:55 +0100

    Changed some definitions and removed priority queue implementation

--- definitions.py
+++ definitions.py
@@ -1,22 +1,16 @@
 from dataclasses import dataclass, astuple
-from typing      import List
+from typing      import List, Any
 
 
 @dataclass
 class Point:
-    x: int = None
-    y: int = None
+    x: int
+    y: int
 
 
 @dataclass
 class Agent:
-    target: Point = None
-    coords: Point = None
-    group:  str   = None
-    state:  str   = None
-
-@dataclass
-class Board:
-    def __init__(self, w: int, h: int):
-        self.content = [[None for _ in h] for _ in w]
-    content: List[List] = None
+    target: Point
+    coords: Point
+    group:  str
+    state:  str
