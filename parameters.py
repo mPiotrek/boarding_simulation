6309188986e02a16569e226199d8e2e73fd16d47
mPiotrek <59826203+mPiotrek@users.noreply.github.com>
2020-01-13 21:14:56 +0100

    Changed parameters design

--- parameters.py
+++ parameters.py
@@ -1,4 +1,9 @@
-width       = 16
-corridor_y  = 3
-height      = 7
-total_seats = width * (height - 1)
+# These should be constant between runs and are considered global
+seats_left   = 3
+seats_right  = 3
+aisle_width  = 1
+plane_width  = seats_left + aisle_width + seats_right
+plane_length = 16
+corridor_y   = 3
+seats_per_row= seats_left + seats_right
+total_seats  = plane_length * seats_per_row
